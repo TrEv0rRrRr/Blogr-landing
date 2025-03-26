8fcf973eb51b2927e202c34b682d39923b745e55
Trevor <valentinosolischang@gmail.com>
2025-03-26 11:12:01 -0500

    Add SECTIONS constant with content and images for blog features

---
+++ src/components/consts/Sections.ts
@@ -0,0 +1,48 @@
+export const SECTIONS = [
+  {
+    imgs: {
+      mobile: "illustations/illustration-editor-mobile.svg",
+      desktop: "illustations/illustration-editor-desktop.svg",
+    },
+    articles: [
+      {
+        heading: "Introducing an extensible editor",
+        description:
+          "Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the look of a blog.",
+      },
+      {
+        heading: "Robust content management",
+        description:
+          "Flexible content management enables users to easily move through posts, increase the usability of your blog by adding customized categories, sections, format, or flow. With this functionality, you're in full control.",
+      },
+    ],
+  },
+  {
+    img: "illustations/illustration-phones.svg",
+    articles: [
+      {
+        heading: "State of the Art Infrastructure",
+        description:
+          "With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.",
+      },
+    ],
+  },
+  {
+    imgs: {
+      mobile: "illustations/illustration-laptop-mobile.svg",
+      desktop: "illustations/illustration-laptop-desktop.svg",
+    },
+    articles: [
+      {
+        heading: "Free, open, simple",
+        description:
+          "Blogr is a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.",
+      },
+      {
+        heading: "Powerful tooling",
+        description:
+          "Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.",
+      },
+    ],
+  },
+];
