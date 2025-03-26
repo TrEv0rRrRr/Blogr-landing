6a71716261ec3780cdf22dbb1a1aa69125a8bfec
Trevor <valentinosolischang@gmail.com>
2025-03-26 18:41:22 -0500

    Add support for customizable padding in Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,16 @@
 type Props = {
   colorStyles: string;
   text: string;
+  differentPadding?: boolean;
 };
 
-const Button = ({ colorStyles, text }: Props) => {
+const Button = ({ colorStyles, text, differentPadding = false }: Props) => {
   return (
     <button
-      className={`cursor-pointer border border-white rounded-4xl px-5 py-2 
-        transition-all ${colorStyles}`}
+      className={`cursor-pointer border border-white rounded-4xl  
+        transition-all ${colorStyles} ${
+        differentPadding ? "px-10 py-2.5" : "px-5 py-2"
+      }`}
     >
       {text}
     </button>
