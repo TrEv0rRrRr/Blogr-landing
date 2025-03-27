e4ae0435c88c4ebc353174ba9d5e651a298580db
Trevor <valentinosolischang@gmail.com>
2025-03-26 19:37:57 -0500

    Enhance Button component with transition duration for smoother interactions

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ const Button = ({ colorStyles, text, differentPadding = false }: Props) => {
   return (
     <button
       className={`cursor-pointer border border-white rounded-4xl  
-        transition-all ${colorStyles} ${
+        transition-all duration-300 ${colorStyles} ${
         differentPadding ? "px-10 py-2.5" : "px-5 py-2"
       }`}
     >
